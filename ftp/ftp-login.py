8a2e0ae6c106eaae4862555f00a76f5f2ad90695
Laurent Pech <laurentpech7@gmail.com>
2021-06-29 14:55:39 +0200

    Added login function to FTP server

---
+++ ftp/ftp-login.py
@@ -0,0 +1,21 @@
+# Script pour les 3 clients FTP AC distants
+
+from ftplib import FTP
+
+# Les paramètres de connexion au serveur FTP
+#ftp_host = '127.0.0.1'
+#ftp_login = 'Laurent'
+#ftp_password = 'G20'
+
+# Connexion au serveur pour chacun des AC
+#ftp = FTP(ftp_host, ftp_login, ftp_password)
+#print(ftp.getwelcome()) # Message de bienvenue
+
+def login():
+    ftp_host = '127.0.0.1'
+    ftp_login = 'Laurent'
+    ftp_password = 'G20'
+    ftp = FTP(ftp_host, ftp_login, ftp_password)
+    print(ftp.getwelcome())
+    return ftp
+ftp=login()
