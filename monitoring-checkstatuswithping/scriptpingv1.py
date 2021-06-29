7865c3ae55584411c68701bf9851022bf046659b
Guillaume Bretagnolle <gbretagnolle@myges.fr>
2021-06-29 16:49:43 +0200

    First version of the device status check with ping

---
+++ monitoring-checkstatuswithping/scriptpingv1.py
@@ -0,0 +1,11 @@
+import subprocess
+import os
+with open(os.devnull, "wb") as limbo:
+        for n in range(200, 240):
+                ip="127.0.0.{0}".format(n)
+                result=subprocess.Popen(["ping", "-n", "1", "-w", "200", ip],
+                        stdout=limbo, stderr=limbo).wait()
+                if result:
+                        print (ip, "inactive")
+                else:
+                        print (ip, "active")
