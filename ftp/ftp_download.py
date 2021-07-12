97731983dcf284bcc65b9749fc96d7ef3632b65a
Laurent Pech <laurentpech7@gmail.com>
2021-07-12 17:01:59 +0200

    Added download function for FTP

---
+++ ftp/ftp_download.py
@@ -0,0 +1,30 @@
+from ftplib import FTP
+import os
+
+import ftp_login
+
+ftp = ftp_login.login()
+
+def download():
+    # Client dossier sauvegarde
+    savedir = "C:/Users/laure/Reception"
+    os.chdir(savedir)
+
+    # Serveur - se mettre dans le bon répertoire
+    ftp.cwd("Test")
+    # Serveur - nom fichier à copier
+    filename = "Test.txt"
+    try:
+        file = open(filename, "wb")
+    except Exception as e:
+        print("Problème lors de l'ouverture du fichier")
+        print("Erreur complète :\n", e)
+    ftp.retrbinary('RETR ' + filename, file.write, 1024)
+    try:
+        file.close()
+    except Exception as e:
+        print("Problème lors de la fermeture du fichier")
+        print("Erreur complète :\n", e)
+    print("Téléchargement terminé")
+    
+download()
