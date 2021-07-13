bb3bc4a45b533dcfdc253af2db83c7b9ed510193
Angelo GERARD <angelogerard@live.fr>
2021-07-13 15:41:20 -0400

    Finally fixed the script to show git stats between two commits

--- script/review_stats.sh
+++ script/review_stats.sh
@@ -5,7 +5,7 @@
 
 set -e
 
-ORA='\033[0;33m'
+ORA='\033[0;33m' # Orange
 NC='\033[0m' # No Color
 
 
@@ -17,26 +17,53 @@ fi
 
 # Le $# revoie le nombre de arguments/paramètres passé au script ./review_stats.sh
 # Si nombre d'argument différent de 2
-if [ $# -lt 2 -o $# -gt 2 ]
+if [ $# -ne 2 ]
   then
-    echo "For this script to work, exactly 2 parameters are required, example : ./review_stats.sh 846d29d cf5956a"
+    echo "For this script to work, exactly 2 parameters are required (Git commit ID/hash), example : ./review_stats.sh 846d29d cf5956a"
     exit 1
-else
-  echo -e "Git repo stats between\n${ORA}$1${NC}\n&\n${ORA}$2${NC}\n"
-fi
-
 # Si 2 arguments
-if [ $# -eq 2 ]
-  then
-    # Pour afficher les changements entre les commits $1 et $2
-    # "tail -n1" permet de garder seulement la dernière ligne en sortie de la commande
-    # "xargs" permet de retirer les espaces en début et fin de chaîne
-    changes=$(echo $(echo -e "$(git diff --stat $1 $2)" | tail -n1) | xargs)
-    # Pour afficher le nombre de commit entre $1 et $2
-    commit=$(git rev-list --count $1 $2)
-    # Pour compter le nombre de pull request qu'il y a eu entre $1 et $2
-    # "wc -l" permet de compter le nombre de ligne
-    merged=$(git log --merges --oneline $1 ^$2^ | wc -l)
+else
+  regexcheckcommit=^[a-f0-9]{7,40}$
+  commitone=$1
+  committwo=$2
+  err=0
+  if ! [[ $commitone =~ $regex && $committwo =~ $regexcheckcommit ]]
+    then
+      echo "Incorrect parameters, these are not Git commit ID/hash, here's an example of parameters : ./review_stats.sh 846d29d cf5956a"
+      exit 1
+  fi
+  if [ ! $(git cat-file -t $commitone 2>/dev/null) ]
+    then
+      echo -e "Error: $commitone is not a correct commit" # >> /dev/stderr
+      err=1
+  fi
+  if [ ! $(git cat-file -t $committwo 2>/dev/null) ]
+    then
+      echo -e "Error: $committwo is not a correct commit" # >> /dev/stderr
+      err=1
+  fi
+  if [[ $err == 1 ]]
+    then
+      exit 1
+  fi
+  timecommitone=$(git show -s --format=%ct $commitone)
+  timecommittwo=$(git show -s --format=%ct $committwo)
+  # Si le commit1 est plus ancien que commit2, alors swap les valeurs
+  if [ "$timecommitone" -lt "$timecommittwo" ]
+    then
+      temp=$commitone
+      commitone=$committwo
+      committwo=$temp
+  fi
+  # Afficher entre quels commits nous faisons la comparaison
+  echo -e "Git repo stats between\n${ORA}${commitone:0:7}${NC} (Newest: $(git show -s --format=%ci $commitone))\n&\n${ORA}${committwo:0:7}${NC} (Oldest: $(git show -s --format=%ci $committwo))\n"
+  # Pour afficher les changements entre les commits 1 et 2
+  changes=$(echo $(echo -e "$(git diff --stat $commitone $committwo)" | tail -n1) | xargs)
+  # Pour afficher le nombre de commit entre 1 et 2
+  commit=$(git rev-list --count $commitone $committwo)
+  # Pour compter le nombre de pull request qu'il y a eu entre 1 et 2
+  # "wc -l" permet de compter le nombre de ligne
+  merged=$(git log --merges --oneline $commitone ^$committwo^ | wc -l)
 fi
 
 echo -n "Number of commits:"
@@ -47,4 +74,3 @@ echo -e "\t$changes" | tail -n1
 
 echo -n "Number of merged PRs:"
 echo -e "\t$merged"
-
