f088022c34e992e17a88d0007cb3875ec92dbc68
LaurentPech <78450385+LaurentPech@users.noreply.github.com>
2021-07-15 00:07:09 +0200

    Update ftp_login.py

--- ftp/ftp_login.py
+++ ftp/ftp_login.py
@@ -2,19 +2,12 @@
 
 from ftplib import FTP
 
-# Les paramètres de connexion au serveur FTP
-#ftp_host = '127.0.0.1'
-#ftp_login = 'Laurent'
-#ftp_password = 'G20'
-
-# Connexion au serveur pour chacun des AC
-#ftp = FTP(ftp_host, ftp_login, ftp_password)
-#print(ftp.getwelcome()) # Message de bienvenue
-
 def login():
+    # Les paramètres de connexion au serveur FTP
     ftp_host = '127.0.0.1'
     ftp_login = 'Laurent'
     ftp_password = 'G20'
+    # Connexion au serveur pour chacun des users
     ftp = FTP(ftp_host, ftp_login, ftp_password)
-    print(ftp.getwelcome())
+    print(ftp.getwelcome()) # Message de bienvenue
     return ftp
